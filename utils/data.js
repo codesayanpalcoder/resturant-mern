8c0fc8015ebc8996739ed519bd2272d6949faa31
codesayanpalcoder <103876082+codesayanpalcoder@users.noreply.github.com>
2023-09-27 15:34:42 +0530

    ch
    ch

---
+++ utils/data.js
@@ -0,0 +1,84 @@
+const items = [
+  {
+    name: "Tea",
+    price: 25,
+    category: "Drinks",
+    image:
+      "https://img.etimg.com/photo/msid-69212931,quality-100/chai-itself-was-once-a-trend-that-developed-this-way-.jpg",
+  },
+  {
+    name: "Sarbat",
+    price: 25,
+    category: "Drinks",
+    image:"https://5.imimg.com/data5/FF/PR/MY-6552690/nimbu-sharbat-masala.jpg",
+  },
+  {
+    name: "Lassi",
+    price: 25,
+    category: "Drinks",
+    image:"https://www.shaadidukaan.com/editor-img/image/food/bengali-wedding-menu/mishti-dahi.jpg",
+  },
+ 
+  {
+    name: "coffee",
+    price: 25,
+    category: "Drinks",
+    image: "https://i.cdn.newsbytesapp.com/images/l110_14211532861690.jpg",
+  },
+  {
+    name: "Chicken Biryani",
+    price: 100,
+    category: "Rice",
+    image:
+      "https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg",
+  },
+  {
+    name: "Veg Biryani",
+    price: 80,
+    category: "Rice",
+    image:
+      "https://www.indianveggiedelight.com/wp-content/uploads/2020/04/veg-biryani-instant-pot-featured.jpg",
+  },
+  {
+    name: "Chicken Hakka",
+    price: 100,
+    category: "Others",
+    image: "https://cravecookclick.com/wp-content/uploads/2012/07/IMG_4400.jpg",
+  },
+  {
+    name: "Fried Rice",
+    price: 100,
+    category: "Rice",
+    image: "https://th.bing.com/th/id/OIP.hPupSi7E4Ajz-vIYZyRnSQHaE7?pid=ImgDet&rs=1",
+  },
+  
+  {
+    name: "Cart Noodles",
+    price:70,
+    category: "Others",
+    image:
+      "https://myhktour.com/wp-content/uploads/2018/04/cart-noodles_bdnw.jpg",
+  },
+  {
+    name: "Ramen Soup",
+    price:70,
+    category: "Others",
+    image:
+      "https://th.bing.com/th/id/R.eb3ed4cecd266da2f3e48f594e4398b1?rik=9%2bfn9PDHdlEAfg&riu=http%3a%2f%2fcdn.bushwickdaily.com%2fpost-cover%2fHHQLuJRiaF2HutNFAoCPjg-default.jpg&ehk=tVnWQe9NkHeN%2fUMgvjJmc0fHgBHpp%2bE9a%2fK%2filXzSfg%3d&risl=&pid=ImgRaw&r=0",
+  },
+  {
+    name: "Veg Soup",
+    price:70,
+    category: "Others",
+    image:
+      "https://th.bing.com/th/id/R.d3e06911e5a556c3f1e070b79f78ec63?rik=skAUhS9BG64P3w&riu=http%3a%2f%2fwww.femalefirst.co.uk%2fimage-library%2fsquare%2f1000%2fs%2fstocksnapzz633je9ot.jpg&ehk=FDNYfDdNUwBNJjBYgbuDeedaQrCEQcU01kEp49InfA8%3d&risl=&pid=ImgRaw&r=0",
+  },
+  {
+    name: "Stream Ramen",
+    price:70,
+    category: "Others",
+    image:
+      "https://64.media.tumblr.com/9653e6a29dc43541e51fb17fc7afeefa/tumblr_n1eue8kwPa1rwj2pqo1_1280.jpg",
+  },
+];
+module.exports = items;
